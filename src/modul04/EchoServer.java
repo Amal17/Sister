ee7653eeadae93ef568f75cb29c39a4d388cb2e8
Amal17 <fadhil.muq@gmail.com>
2018-09-30 12:39:04 +0700

    Modul 04

---
+++ src/modul04/EchoServer.java
@@ -0,0 +1,40 @@
+package modul04;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class EchoServer {
+
+    public static final int SERVICE_PORT = 7;
+    public static final int BUFSIZE = 4096;
+    private DatagramSocket socket;
+    
+    public EchoServer(){
+        try{
+            socket = new DatagramSocket(SERVICE_PORT);
+            System.out.println("Server Active on port " + socket.getLocalPort());
+        }catch (Exception e){
+            System.err.println("Unable to bind port "+e);
+        }
+    }
+    
+    public void serviceClients(){
+        byte[] buffer = new byte[BUFSIZE];
+        for(;;){
+            try{
+                DatagramPacket packet = new DatagramPacket(buffer, BUFSIZE);
+                socket.receive(packet);
+                System.out.println("Packet received from "+packet.getAddress()+":"+packet.getPort()+" of length "+packet.getLength());
+                socket.send(packet);
+            }catch(IOException e){
+                System.err.println("I/O Exception : "+e);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        EchoServer server = new EchoServer();
+        server.serviceClients();
+    }
+}
---
+++ src/modul04/SimplePacketReceive.java
@@ -0,0 +1,21 @@
+package modul04;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+/**
+ *
+ * @author alfa
+ */
+public class SimplePacketReceive {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(2000);
+        
+        DatagramPacket packet = new DatagramPacket(new byte[256], 256);
+        socket.receive(packet);
+        
+        String message = new String(packet.getData(), 0, packet.getLength());
+        System.out.println(message);
+    }
+}
